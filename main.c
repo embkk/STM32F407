62629873e056e0d14a517628c723f9f8b7b5b97c
K K <gpflv@ya.ru>
2025-12-02 14:58:26 +0300

    ADC1 init

--- main.c
+++ main.c
@@ -14,9 +14,6 @@ int main(void) {
   RCC_Init();
   SysTick_Config(84000);
 
-  //Buttons_init();
-  //EXTI_init_lines(10,11);
-  //LED_init();
   TIM1_Init();
   ADC1_Init();
 
@@ -31,20 +28,27 @@ void ADC_IRQHandler(void) {
 }
 
 void ADC1_Init(void) {
-  RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;   
-  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+  RCC->APB2ENR |= RCC_APB2ENR_ADC1EN; // Включение тактирования АЦП
+  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN; // Включение тактирования порта GPIOA
 
-  GPIOA->MODER |= GPIO_MODER_MODE5;
-  
-  ADC1->SMPR2  |= ADC_SMPR2_SMP5_0;       // 15+12 циклов
-  ADC1->SQR1   |= ~(ADC_SQR1_L);          // Длина последовательности 1
-  ADC1->SQR3   |= 5 << ADC_SQR3_SQ1_Pos;  // Первая конвертация 5 канал
-  ADC1->CR1    |= ADC_CR1_EOCIE;          // прерывание по завершению преобразования
-  
-  NVIC_EnableIRQ(ADC_IRQn);
+  GPIOA->MODER |= GPIO_MODER_MODE5; // GPIOA_PA5 в режиме альтен. функции (ADC1_CH5)
 
-  ADC1->CR2    |= ADC_CR2_CONT | ADC_CR2_ADON;   //непрерывный режим | включаем модуль ацп
-  ADC1->CR2    |= ADC_CR2_SWSTART;//запуск измерения
+  ADC1->SMPR2 |= ADC_SMPR2_SMP5_0; // выбираем канал конвертирования ADC1 Channel 5 и время конвертирования 15 тактов
+  ADC1->JSQR &= ~(ADC_JSQR_JL); // Длина последовательности инжектированных каналов равна 1
+  ADC1->JSQR |= (5 << ADC_JSQR_JSQ4_Pos); // выбрать АЦП канал 5 для конвертирования в последовательности
+
+  // Разрядность 10^2 = 1024  
+  ADC1->CR1 |= ADC_CR1_RES_0; // Выбрали 10-бит разрядность АЦП
+  
+  ADC1->CR1 |= ADC_CR1_JAWDEN; // Включили Analog Watchdog для инжектированных каналов
+  ADC1->CR1 |= (5 << ADC_CR1_AWDCH_Pos); // Для канала 5 работает Analog Watchdog
+  
+  ADC1->HTR = 819; // 80% Задали верхний порог срабатывания Analog Watchdog
+  ADC1->LTR = 102; // 10% Задали нижний порог срабатывания Analog Watchdog
+
+  ADC1->CR2 |= ADC_CR2_JEXTEN_1; // Включен внешний тригер для запуска конвертации АЦП
+  ADC1->CR2 |= ADC_CR2_JEXTSEL_1;
+  ADC1->CR2 |= ADC_CR2_ADON; // АЦП включен
 }
 
 void TIM1_Init(void) {
