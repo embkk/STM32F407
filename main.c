1f68d640a6957190dd7b32084076a556f9b726f4
K K <gpflv@ya.ru>
2025-12-03 12:07:16 +0300

    Work in progress

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include "log.h"
 
 void RCC_Init(void);
-void TIM1_Init(void);
+void TIM2_Init(void);
 void ADC1_Init(void);
 void ADC_IRQHandler(void);
 
@@ -14,7 +14,7 @@ int main(void) {
   RCC_Init();
   SysTick_Config(84000);
 
-  TIM1_Init();
+  TIM2_Init();
   ADC1_Init();
 
   while(1) {
@@ -23,53 +23,66 @@ int main(void) {
 }
 
 void ADC_IRQHandler(void) {
-  TIM1->CCR4 = ( ADC1->DR * 1000 ) / 4096;
-  NVIC_ClearPendingIRQ(ADC_IRQn);
+  LOG_MESSAGE("IRQ ");
 }
 
 void ADC1_Init(void) {
   RCC->APB2ENR |= RCC_APB2ENR_ADC1EN; // Включение тактирования АЦП
   RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN; // Включение тактирования порта GPIOA
 
-  GPIOA->MODER |= GPIO_MODER_MODE5; // GPIOA_PA5 в режиме альтен. функции (ADC1_CH5)
+  GPIOA->MODER |= GPIO_MODER_MODE5; // GPIOA_PA5 в режиме аналог (ADC1_CH5)
 
   ADC1->SMPR2 |= ADC_SMPR2_SMP5_0; // выбираем канал конвертирования ADC1 Channel 5 и время конвертирования 15 тактов
   ADC1->JSQR &= ~(ADC_JSQR_JL); // Длина последовательности инжектированных каналов равна 1
   ADC1->JSQR |= (5 << ADC_JSQR_JSQ4_Pos); // выбрать АЦП канал 5 для конвертирования в последовательности
 
-  // Разрядность 10^2 = 1024  
-  ADC1->CR1 |= ADC_CR1_RES_0; // Выбрали 10-бит разрядность АЦП
   
-  ADC1->CR1 |= ADC_CR1_JAWDEN; // Включили Analog Watchdog для инжектированных каналов
-  ADC1->CR1 |= (5 << ADC_CR1_AWDCH_Pos); // Для канала 5 работает Analog Watchdog
+  ADC1->CR1 |= ADC_CR1_JAWDEN; // Analog Watchdog для инжектированных каналов
+  ADC1->CR1 |= (5 << ADC_CR1_AWDCH_Pos); // канал 5 Analog Watchdog
+  ADC1->CR1 |= ADC_CR1_AWDIE; // разрешение прерывания по analog watchdog
+  ADC1->CR1 |= ADC_CR1_JEOCIE; // разрешение прерывания при injected end of conversion
   
-  ADC1->HTR = 819; // 80% Задали верхний порог срабатывания Analog Watchdog
-  ADC1->LTR = 102; // 10% Задали нижний порог срабатывания Analog Watchdog
+  ADC1->CR1 |= ADC_CR1_RES_0; // 10 бит 1024
+  ADC1->HTR = 819; // 80% от 1024
+  ADC1->LTR = 102; // 10% от 1024
+  
+  ADC1->CR2 |= ADC_CR2_JEXTEN_1; // falling edge
+  ADC1->CR2 |= 0b0110 << ADC_CR2_JEXTSEL_Pos; // TIM2_TRGO
+  ADC1->CR2 |= ADC_CR2_ADON; // АЦП вкл
 
-  ADC1->CR2 |= ADC_CR2_JEXTEN_1; // Включен внешний тригер для запуска конвертации АЦП
-  ADC1->CR2 |= ADC_CR2_JEXTSEL_1;
-  ADC1->CR2 |= ADC_CR2_ADON; // АЦП включен
+  NVIC_EnableIRQ(ADC_IRQn);
 }
 
-void TIM1_Init(void) {
-  RCC->APB2ENR |= RCC_APB2ENR_TIM1EN;
-  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOEEN;
+void TIM2_Init(void)
+{
+    // When using triggered injection, one must ensure that the interval between trigger events is
+    // longer than the injection sequence.
 
-  GPIOE->MODER   |= GPIO_MODER_MODE14_1;
-  GPIOE->OSPEEDR |= GPIO_OSPEEDER_OSPEEDR14_1;
-  GPIOE->AFR[1]  |= GPIO_AFRH_AFRH6_0;
-  
-  // Захват входа
-  TIM1->PSC    = 83;
-  TIM1->CR1   |= TIM_CR1_CMS;
-  TIM1->ARR    = 999;
-  TIM1->CCR4   = 1;
-  TIM1->CCMR2 |= TIM_CCMR2_OC4M;
-  TIM1->CCMR2 &= ~(TIM_CCMR2_CC4S);
-  TIM1->CCER  |= TIM_CCER_CC4E;
-  TIM1->BDTR  |= TIM_BDTR_MOE;
+    RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;   // тактирование таймера
 
-  TIM1->CR1 |= TIM_CR1_CEN;
+    TIM2->PSC = 83;               // 84 MHz → 1 MHz
+    TIM2->ARR = 999;              // период 1 мс (1000 тактов)
 
-  TIM1->EGR |= TIM_EGR_UG;
+    // 010: Update - The update event is selected as trigger output (TRGO).
+    // For instance a master timer can then be used as a prescaler for a slave timer.
+    TIM2->CR2 &= ~TIM_CR2_MMS;    
+    TIM2->CR2 |= 0b010 << TIM_CR2_MMS_Pos;
+
+    TIM2->DIER |= TIM_DIER_UIE; // Bit 0 UIE: Update interrupt enable
+
+    /* Bit 0 UG: Update generation 1: Re-initialize the counter and generates an update of the registers. */
+    TIM2->EGR |= TIM_EGR_UG;
+    
+    TIM2->CR1 |= TIM_CR1_CEN;
+
+    NVIC_EnableIRQ(TIM2_IRQn);
 }
+/*void TIM2_IRQHandler(void)
+{
+    if (TIM2->SR & TIM_SR_UIF)
+    {
+        TIM2->SR &= ~TIM_SR_UIF;
+        LOG_MESSAGE("TIM2\n");
+    }
+}
+*/
