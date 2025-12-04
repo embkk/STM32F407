e66b94c310234f984059a4f58661e5c6670663da
K K <gpflv@ya.ru>
2025-12-04 18:07:46 +0300

    configured as lecture

--- main.c
+++ main.c
@@ -35,13 +35,14 @@ void ADC1_Init(void) {
   RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
 
   GPIOA->MODER |= GPIO_MODER_MODE5;
+
+  ADC->CCR |= ADC_CCR_ADCPRE; // pclk2/8
   
-  ADC1->SMPR2  |= ADC_SMPR2_SMP5_0;       // 15+12 циклов
-  ADC1->SQR1   |= ~(ADC_SQR1_L);          // Длина последовательности 1
+  ADC1->SMPR2  |= ADC_SMPR2_SMP5_2;       // 84+12 циклов
+  ADC1->SQR1   |= 0 << ADC_SQR1_L_Pos;    // Длина последовательности 1
   ADC1->SQR3   |= 5 << ADC_SQR3_SQ1_Pos;  // Первая конвертация 5 канал
-  ADC1->CR1    |= ADC_CR1_EOCIE;          // прерывание по завершению преобразования
   
-  NVIC_EnableIRQ(ADC_IRQn);
+  ADC1->CR2    |= ADC_CR2_DMA | ADC_CR2_DDS;  //dma 
 
   ADC1->CR2    |= ADC_CR2_CONT | ADC_CR2_ADON;   //непрерывный режим | включаем модуль ацп
   ADC1->CR2    |= ADC_CR2_SWSTART;//запуск измерения
@@ -51,18 +52,22 @@ void TIM1_Init(void) {
   RCC->APB2ENR |= RCC_APB2ENR_TIM1EN;
   RCC->AHB1ENR |= RCC_AHB1ENR_GPIOEEN;
 
-  GPIOE->MODER   |= GPIO_MODER_MODE14_1;
+  /*GPIOE->MODER   |= GPIO_MODER_MODE14_1;
   GPIOE->OSPEEDR |= GPIO_OSPEEDER_OSPEEDR14_1;
-  GPIOE->AFR[1]  |= GPIO_AFRH_AFRH6_0;
+  GPIOE->AFR[1]  |= GPIO_AFRH_AFRH6_0;*/
   
+  GPIOE->MODER   |= GPIO_MODER_MODE13_1;
+  GPIOE->OSPEEDR |= GPIO_OSPEEDER_OSPEEDR13_1;
+  GPIOE->AFR[1]  |= GPIO_AFRH_AFRH5_0;
+
   // Захват входа
   TIM1->PSC    = 83;
   TIM1->CR1   |= TIM_CR1_CMS;
   TIM1->ARR    = 999;
   TIM1->CCR4   = 1;
-  TIM1->CCMR2 |= TIM_CCMR2_OC4M;
-  TIM1->CCMR2 &= ~(TIM_CCMR2_CC4S);
-  TIM1->CCER  |= TIM_CCER_CC4E;
+  TIM1->CCMR2 |= TIM_CCMR2_OC3M;
+  TIM1->CCMR2 &= ~(TIM_CCMR2_CC3S);
+  TIM1->CCER  |= TIM_CCER_CC3E;
   TIM1->BDTR  |= TIM_BDTR_MOE;
 
   TIM1->CR1 |= TIM_CR1_CEN;
