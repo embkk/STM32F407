bba363d3f55f9fa44e7ae46d6d7bd238bf25191b
K K <gpflv@ya.ru>
2025-12-26 02:01:04 +0300

    Clear project

--- main.c
+++ main.c
@@ -1,31 +1,8 @@
 #include <stm32f407xx.h>
 #include "log.h"
 #include "delay.h"
-#include "buttons.h"
-#include "usart.h"
-#include "modbus_rtu.h"
-#include "adc.h"
-
 void RCC_Init(void);
 
-uint8_t modbus_req_rx[256];
-uint8_t modbus_rx_len;
-uint8_t modbus_err;
-uint8_t modbus_answer_tx[256];
-uint8_t modbus_answer_len;
-
-void USART6_IRQHandler(void) {
-  if(USART6->SR & USART_SR_RXNE) {
-    ModbusReception();
-  }
-  NVIC_ClearPendingIRQ(USART6_IRQn);
-}
-
-void TIM2_IRQHandler(void) {
-  TIM2->SR &= ~(TIM_SR_UIF);
-  ModbusTimersIRQ();
-  NVIC_ClearPendingIRQ(TIM2_IRQn);
-}
 
 int main(void) {
   LOG_INIT();
@@ -33,26 +10,9 @@ int main(void) {
   SystemInit();
   RCC_Init();
 
-  __enable_irq();
-
-  LED_init();
-  Buttons_init();
-  
-  TIM2_init();
-  USART6_init();
-  ADC1_Init();
-
   SysTick_Config(SYSTICK_TIMER_CONST);
 
-  GPIO_LED_all_off();
-
-  ModbusTimerStart(DELAY_3_5_BYTE_US);
-
   while(1) {
-    modbus_err = RequestParsingOperationExec();
-    if(modbus_err!=0) {
-      LOG_MESSAGE("Modbus 0x%X adc %d", modbus_err, ADC1->DR * 1000);
-    }
   }
 }
 
