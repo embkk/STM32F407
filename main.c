d21e25628c7f1b8b4e1b13046c35b51a857ce1c8
K K <gpflv@ya.ru>
2026-01-04 04:36:01 +0300

    Working buffers

--- main.c
+++ main.c
@@ -1,23 +1,57 @@
 #include "main.h"
   
-
-
 #define CMD_START "_start_"
 #define CMD_STOP "_stop_"
 
-uint8_t state_writestream = 0;
 uint8_t state_started = 0;
+uint8_t state_cmd = 0;
 
-char input_buffer[MAX_BYTES_TO_READ];
+char input_buffer[MAX_BYTES_TO_READ] ={'\0'};
+char output_buffer[MAX_BYTES_TO_READ] ={'\0'};
+uint8_t input_buffer_len;
+uint8_t output_buffer_len;
+
+
+uint8_t check_cmd(const char* cmd, uint8_t cmd_len ) {
+  for (int i = 0; i < cmd_len-1; i++) {
+    if(i>=input_buffer_len) {
+      return 2; // Part of a command
+    }
+    if (cmd[i] != input_buffer[i]) {
+      return 1; // Not a command
+    }
+  }
+  //LOG_MESSAGE("True");
+  return 0; // Complete command
+}
+
+uint8_t parse_buffer_char(char c) {
+  //LOG_MESSAGE("[%d] Input buffer %s", buffer_len, input_buffer);
+  uint8_t cmd_status = 1; // Not a command
+  if(!state_started) {
+    cmd_status = check_cmd(CMD_START, sizeof(CMD_START));
+    if(cmd_status == 0) {
+      LOG_MESSAGE("CMD: _start_");
+      state_started = 1;
+    }
+  } else {
+    cmd_status = check_cmd(CMD_STOP, sizeof(CMD_STOP));
+    if(cmd_status == 0) {
+      LOG_MESSAGE("CMD: _stop_");
+      state_started = 0;
+    }
+  }
+
+  //LOG_MESSAGE("%s, check_cmd = %d, state_cmd = %d", state_started ? "Started" : "Not started", cmd_status, state_cmd);
+  return cmd_status;
+}
 
 void USART1_IRQHandler(void) {
-    if (USART1->SR & USART_SR_RXNE) {
-        uint8_t data = USART1->DR; // Читаем данные, это автоматически сбросит флаг прерывания
-        if(!state_started) {
-          
-        }
-    }
-    NVIC_ClearPendingIRQ(USART1_IRQn);
+  if (USART1->SR & USART_SR_RXNE) {
+    input_buffer[input_buffer_len] = USART1->DR;
+    input_buffer_len++;
+  }
+  NVIC_ClearPendingIRQ(USART1_IRQn);
 }
 
 
@@ -72,8 +106,28 @@ int main(void) {
     printf("----- SD-card not found! -----\n");
   }
 
-
+  uint8_t parse_result;
+  
   while(1) {
+    for(int i=0; i<input_buffer_len;i++) {
+      parse_result = parse_buffer_char(input_buffer[i]);
+
+      if(parse_result == 0) {
+        //command
+        input_buffer_len = 0;
+        break;
+      } else if(state_started && parse_result==1) {
+        //write a symbol
+        output_buffer[output_buffer_len] = input_buffer[i];
+        output_buffer_len++;
+        input_buffer_len--;
+      }
+    }
+
+    for(int i=0; i<output_buffer_len; i++ ) {
+      LOG_MESSAGE("Writen %c", output_buffer[i]);
+      output_buffer_len = 0;
+    }
   }
 }
 
